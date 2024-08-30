56c029ec950c36f8ddd36491c7b271639045fd8e
Hugo Meneses <hugomeneses86@gmail.com>
2024-08-30 17:24:08 -0500

    Primer commit en nueva rama

---
+++ app/src/main/java/com/example/my_appvideo/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.my_appvideo
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val videoView: VideoView = findViewById(R.id.videoView)
+
+        val videoUri = Uri.parse("android.resource://${packageName}/${R.raw.video_1}")
+        videoView.setVideoURI(videoUri)
+
+        val mediaController  =MediaController(this)
+        videoView.setMediaController(mediaController)
+
+
+        videoView.start()
+
+
+    }
+}
